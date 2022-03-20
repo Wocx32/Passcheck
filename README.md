10bea5bcace0900615068398ffa4a97e0733af65
Wocx-32 <63423836+Wocx-32@users.noreply.github.com>
2022-03-20 18:02:40 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Passcheck
+A password checker which utilizes the haveibeenpwned api
+
+_Currently only supports csv files_
+
+# Install
+Clone this repository
+
+    git clone https://github.com/Wocx32/Passcheck.git
+
+Install the requirements
+
+    pip install -r requirements.txt
+
+or
+
+    pip3 install -r requirements.txt
+
+# Usage
+
+Provide a csv file containing url, username and password columns as commandline argument
+
+    python3 main.py example.csv
+
+or
+
+    python main.py example.csv
+
+
+![Example](/.resources/example.png)
+
+_Note: It has been tested with csv files exported by Chrome and Firefox_
+
+# Requirements
+- [Python3](https://www.python.org/)
+- [rich](https://github.com/Textualize/rich)
---
+++ main.py
@@ -0,0 +1,58 @@
+from time import sleep
+import requests
+import hashlib
+import csv
+from rich.progress import track
+import sys
+
+
+def request_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url, headers={'Add-Padding': 'true'})
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching: {res.status_code}, check api and try again.")
+    return res
+
+def get_leak_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_data(first5_char)
+    return get_leak_count(response, tail)
+
+def main(file):
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        entries = -1  # first row not included
+        for row in reader:
+            entries += 1
+
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        url_index = None
+        username_index = None
+        password_index = None
+
+        for row in reader:
+            url_index = row.index('url')
+            username_index = row.index('username')
+            password_index = row.index('password')
+            break
+
+        for row in track(reader, description="Processing...", total=entries):
+            result = pwned_api_check(row[password_index])
+
+            print(f"Password for user: {row[username_index]}, on site: {row[url_index]}, appeared {result} times")
+            sleep(1.5)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("No file provided")
+
+    sys.exit(main(sys.argv[1]))
